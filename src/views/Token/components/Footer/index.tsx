b3423c40a227896bf1cd85ae88bf6acc576b6305
ccc <ccc185416@gmail.com>
2025-03-17 14:04:21 +0800

    1

--- src/views/Token/components/Footer/index.tsx
+++
@@ -1,7 +0,0 @@
-import { memo } from 'react'
-
-function App() {
-  return <div>App</div>
-}
-
-export default memo(App)
--- src/views/Token/components/Token_Tool/index.tsx
+++
@@ -1,7 +0,0 @@
-import { memo } from 'react'
-
-function App() {
-  return <div>tool</div>
-}
-
-export default memo(App)

e9cdef49d100546c50c3efc8307c0a8e9c981355
ccc <ccc185416@gmail.com>
2025-03-17 13:12:18 +0800

    1

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || '',
+  siteUrl: process.env.SITE_URL || 'https://www.gtokentool.com',
   generateRobotsTxt: true, // (optional)
   // ...other options
 }

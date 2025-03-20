cf667f522634953cf5335e9a705e219f8cbed624
ccc <ccc185416@gmail.com>
2025-03-20 18:48:39 +0800

    1

--- next.config.js
+++ next.config.js
@@ -138,4 +138,4 @@ const config = {
   },
 }
 
-module.exports = withBundleAnalyzer(withSentryConfig(withTM(withAxiom(config))))
+module.exports = withBundleAnalyzer(withSentryConfig(withTM(withAxiom(config)), sentryWebpackPluginOptions))
